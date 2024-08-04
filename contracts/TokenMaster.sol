5b5ed869c8664986d2df77026499e24e4b0daa87
Saddam Hussain <syncorb2022@gmail.com>
2024-08-04 21:53:27 +0530

    Lisiting the Events

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -6,6 +6,27 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TokenMaster is ERC721  {
 
     address public owner;
+    uint256 public totalOccasions;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+
+    modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+    }
+
+    mapping(uint256 => Occasion) occasions;
+
 
     constructor
     (
@@ -17,5 +38,35 @@ contract TokenMaster is ERC721  {
 
         owner = msg.sender;
 
+    }
+
+
+    
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner {
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+
+
+
+     function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
     }
 }
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,43 +1,82 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("TokenMaster", () => {
-    let tokenMaster;
-
-    let deployer,user;
-
-    let NAME = "TokenMaster";
-    let SYMBOL = "TK";
-
-    describe("Deployment",() => {
-
-    beforeEach(async () => {
 
 
-        [deployer,user] = await ethers.getSigners();
+let NAME = "TokenMaster";
+let SYMBOL = "TK";
+
+const OCCASION_NAME = "ETH Texas"
+const OCCASION_COST = ethers.utils.parseUnits('1', 'ether')
+const OCCASION_MAX_TICKETS = 100
+const OCCASION_DATE = "Apr 27"
+const OCCASION_TIME = "10:00AM CST"
+const OCCASION_LOCATION = "Austin, Texas"
+
+
+    describe("TokenMaster", () => {
+        let tokenMaster;
+
+        let deployer,user;
         
-        const TokenMaster = await ethers.getContractFactory("TokenMaster");
-        tokenMaster = await TokenMaster.deploy(NAME,SYMBOL);
+
+        describe("Deployment",() => {
+
+        beforeEach(async () => {
 
 
+            [deployer,user] = await ethers.getSigners();
+            
+            const TokenMaster = await ethers.getContractFactory("TokenMaster");
+            tokenMaster = await TokenMaster.deploy(NAME,SYMBOL);
+
+            const transaction = await tokenMaster.connect(deployer).list(
+                OCCASION_NAME,
+                OCCASION_COST,
+                OCCASION_MAX_TICKETS,
+                OCCASION_DATE,
+                OCCASION_TIME,
+                OCCASION_LOCATION
+            )
+        
+            await transaction.wait()
+
+
+        })
+
+        it("Sete the name", async () => {
+            let name = await tokenMaster.name();
+            expect(name).to.equal(NAME)
+        })
+
+        it("Sets the symbol", async () => {
+            let symbol = await tokenMaster.symbol();
+            expect(symbol).to.equal(SYMBOL)
+        })
+
+        it("Sets the Owner", async () => {
+            let owner = await tokenMaster.owner();
+            expect(owner).to.equal(deployer.address);
+        })
 
     })
 
-    it("Sete the name", async () => {
-        let name = await tokenMaster.name();
-        expect(name).to.equal(NAME)
+    describe("Occasions", () => {
+        it('Returns occasions attributes', async () => {
+          const occasion = await tokenMaster.getOccasion(1)
+          expect(occasion.id).to.be.equal(1)
+          expect(occasion.name).to.be.equal(OCCASION_NAME)
+          expect(occasion.cost).to.be.equal(OCCASION_COST)
+          expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS)
+          expect(occasion.date).to.be.equal(OCCASION_DATE)
+          expect(occasion.time).to.be.equal(OCCASION_TIME)
+          expect(occasion.location).to.be.equal(OCCASION_LOCATION)
+        })
+    
+        it('Updates occasions count', async () => {
+          const totalOccasions = await tokenMaster.totalOccasions()
+          expect(totalOccasions).to.be.equal(1)
+        })
     })
-
-    it("Sets the symbol", async () => {
-        let symbol = await tokenMaster.symbol();
-        expect(symbol).to.equal(SYMBOL)
-    })
-
-    it("Sets the Owner", async () => {
-        let owner = await tokenMaster.owner();
-        expect(owner).to.equal(deployer.address);
-    })
-
-})
 
 })
